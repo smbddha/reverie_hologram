d620e79addc5f09be15a6ae4ae03259b276669a8
smbddha <samuelwlaing@gmail.com>
2017-01-03 14:51:20 -0500

    Changes to be committed: 	modified:   Scripts/motion.py

--- Scripts/motion.py
+++ Scripts/motion.py
@@ -1,39 +1,64 @@
 #!/usr/env/bin python
+#############################################################################
+#                                                                           #
+#                                                                           #
+# motion.py                                                                 #
+#                                                                           #
+# Converts footage to png sequence then imports it into                     #
+# Adobe After Effects, where Mocha must then be open to motion              #
+# track the actor in the footage                                            #
+#                                                                           #
+#############################################################################
 
-import sys  
+import sys
 import subprocess
 import os
 import ffmpy
+import re
 from shutil import copyfile, rmtree
 
-fpath = "../Template Comp.mov"
-dest = '../output/'
-scriptPath = "c:/Program Files/Adobe/Adobe After Effects CC 2017/Support Files/Scripts/Startup/ReverieScript.jsx"
-aePath = "c:/Program Files/Adobe/Adobe After Effects CC 2017/Support Files/AfterFX.exe"
+OP_SYSTEM = sys.platform
+
+fpath = '../Template Comp.mov'
+outputDestination = '../output/'
+scriptDestination = '/Support Files/Scripts/Startup/ReverieScript.jsx'
+rootAdobePath = 'c:/Program Files/Adobe/' if OP_SYSTEM == 'win32' else '/Applications/Adobe/'
 
 def main():
-	convert(fpath)
-	copyfile("ReverieScript.jsx", scriptPath)
-	aeprocess = subprocess.Popen([aePath])
+    convert(fpath)
+    copyfile("ReverieScript.jsx", rootAdobePath + scriptPath)
+    aeprocess = subprocess.Popen([aePath])
 
-	"""
-	if file[-3:] != '.png':
-		file = convert(file)
-	""" 
-		
+
+def getAfterEffectsPath():
+
+    pattern = re.compile("After Effects")
+
+    for subdir in [d for d in os.listdir(rootAdobePath) if os.path.isdir(rootAdobePath + f)]:
+        if pattern.search(d):
+            print "[+] Found After Effects at {}".format(rootAdobePath + d)
+            return rootAdobePath + d
+
+    print "[-] Adobe After Effects not found"
+    return
+
+
 def convert(infile):
-	os.mkdir(dest)
-	ff = ffmpy.FFmpeg(
-		inputs={infile: None},
-		outputs={dest + 'image-%03d.png':None}
-	)
-	ff.run()
+    os.mkdir(dest)
+    ff = ffmpy.FFmpeg(
+	inputs={infile: None},
+	outputs={dest + 'image-%03d.png':None}
+    )
+
+    print "[+] Converting footage to a png image sequence"
+    ff.run()
 
 def cleanup():
-	try:
-		rmtree(dest)
-	except:
-		print "[+] Error deleting image sequence"
+    try:
+        rmtree(dest)
+    except:
+	print "[-] Error deleting image sequence"
+
 
 if __name__ == '__main__':
 	main()
